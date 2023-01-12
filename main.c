e0270d8d12960f9730e596d3de94c8dea297737d
MK-nn <okuken99@gmail.com>
2023-01-12 21:27:06 +0900

    add main.c

---
+++ main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int map[5][5];
+
+void initialize()
+{
+  for (int y = 0; y < 5; y++)
+  {
+    for (int x = 0; x < 5; x++)
+    {
+      map[y][x] = -1;
+    }
+  }
+}
+
+void printMap(int x_size, int y_size)
+{
+  printf("  x 1 2 3 4 5\n");
+  printf("y - - - - - - -\n");
+  for (int y = 0; y < y_size; y++)
+  {
+    printf("%d |", y + 1);
+    for (int x = 0; x < x_size; x++)
+    {
+      if (map[y][x] >= 0)
+      {
+        printf(" %d", map[y][x]);
+      }
+      else
+      {
+        printf("  ");
+      }
+    }
+    printf(" |\n");
+    printf("  - - - - - - -\n");
+  }
+  printf("\n");
+}
+
+int main(){
+  initialize();
+  printMap(5, 5);
+}
